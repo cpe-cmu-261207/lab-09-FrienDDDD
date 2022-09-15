c381f571a30a121159ecc0b1f915e018cdcab954
FrienDDDD <friendjunk@gmail.com>
2022-09-15 19:24:29 +0700

    update lab-07

--- pages/lab-07.jsx
+++ pages/lab-07.jsx
@@ -1,5 +1,135 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Todo from "../components/Todo";
+import {
+  IconCheck,
+  IconTrash,
+  IconArrowUp,
+  IconArrowDown,
+} from "@tabler/icons";
+import Navbar from "../components/Navbar";
 
-export default function Lab07() {
-  return <div></div>;
+export default function Home() {
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    saveTodos();
+  }, [todos]);
+
+  useEffect(() => {
+    const todoStr = localStorage.getItem("react-todos");
+    if (todoStr === null) {
+      setTodos([]);
+    } else {
+      setTodos(JSON.parse(todoStr));
+    }
+  }, []);
+
+  const onKeyUpHandler = (event) => {
+    if (event.key !== "Enter") return;
+
+    if (todoInput === "") {
+      alert("Todo cannot be empty");
+    } else {
+      const newTodos = [{ title: todoInput, completed: false }, ...todos];
+      setTodos(newTodos);
+      setTodoInput("");
+    }
+  };
+
+  const deleteTodo = (idx) => {
+    todos.splice(idx, 1);
+    const newTodos = [...todos];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (idx) => {
+    todos[idx].completed = !todos[idx].completed;
+    setTodos([...todos]);
+  };
+
+  const moveUp = (idx) => {
+    if (idx === 0) return;
+    const title = todos[idx].title;
+    const completed = todos[idx].completed;
+
+    todos[idx].title = todos[idx - 1].title;
+    todos[idx].completed = todos[idx - 1].completed;
+
+    todos[idx - 1].title = title;
+    todos[idx - 1].completed = completed;
+    setTodos([...todos]);
+  };
+
+  const moveDown = (idx) => {
+    if (idx + 1 === todos.length) return;
+    const title = todos[idx].title;
+    const completed = todos[idx].completed;
+
+    todos[idx].title = todos[idx + 1].title;
+    todos[idx].completed = todos[idx + 1].completed;
+
+    todos[idx + 1].title = title;
+    todos[idx + 1].completed = completed;
+    setTodos([...todos]);
+  };
+
+  const saveTodos = () => {
+    const todosStr = JSON.stringify(todos);
+    localStorage.setItem("react-todos", todosStr);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      {/* Entire App container (required for centering) */}
+      <div style={{ maxWidth: "700px" }} className="mx-auto">
+        {/* App header */}
+        <p className="display-4 text-center fst-italic m-4">
+          Minimal Todo List <span className="fst-normal">☑️</span>
+        </p>
+        {/* Input */}
+        <input
+          className="form-control mb-1 fs-4"
+          placeholder="insert todo here..."
+          onChange={(e) => setTodoInput(e.target.value)}
+          value={todoInput}
+          onKeyUp={onKeyUpHandler}
+        />
+        <div>
+          {todos.map((todo, i) => (
+            <Todo
+              title={todo.title}
+              completed={todo.completed}
+              onMark={() => markTodo(i)}
+              onDelete={() => deleteTodo(i)}
+              onMoveUp={() => moveUp(i)}
+              onMoveDown={() => moveDown(i)}
+            />
+          ))}
+        </div>
+
+        {/* summary section */}
+        <p className="text-center fs-4">
+          <span className="text-primary">All ({todos.length}) </span>
+          <span className="text-warning">
+            Pending ({todos.filter((x) => x.completed === false).length})
+          </span>
+          <span className="text-success">
+            Completed ({todos.filter((x) => x.completed === true).length})
+          </span>
+        </p>
+
+        {/* Made by section */}
+        <p className="text-center mt-3 text-muted fst-italic">
+          made by Kasiyot Hanphairot 640610621
+        </p>
+      </div>
+    </div>
+  );
 }
