e2db9d7fa51cd356699fdae9ae7f5eaa5201444d
FrienDDDD <friendjunk@gmail.com>
2022-09-15 19:23:06 +0700

    add footer

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div style={{ maxWidth: "600px" }} className="mx-auto">
+      <hr />
+      <div className="text-muted text-center">Copyright © kasiyot 2022</div>
+    </div>
+  );
+}
